593640dbe50025380f6fa264af546ad99f2cf297
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-16 16:40:00 +0800

    hashid

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,12 +6,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class ProfileController extends Controller
 {
     //
-    public function show($id){
-        $record = Student::findOrFail($id);
+    public function show($hash){
+        try {
+            $id = Crypt::decryptString($hash);
+            $record = Student::findOrFail($id);
+        } catch (\Exception $e) {
+            // Handle the error if decryption fails or record is not found
+            abort(404, 'Record not found');
+        }
+
         return view('profile.show', compact('record'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ Route::get('/',[StudentController::class,'welcome']);
 Route::resource('/students', StudentController::class);
 Route::post('/updatestudent',[StudentController::class,'updatestudent']);
 
-Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('profile.show');
+Route::get('/profile/{hash}', [ProfileController::class, 'show'])->name('profile.show');
 
 // Route::post('/student', [StudentformController::class, 'createstudent']);
