92fde58c443bfc7e441c0954160a3b138c87e8f0
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-16 17:39:12 +0800

    hashid

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -37,7 +37,7 @@ class FilamentServiceProvider extends ServiceProvider
     {
         Panel::configureUsing(function (Panel $panel) {
             $panel
-            ->spa(false)
+            ->spa()
             ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::Sky,
