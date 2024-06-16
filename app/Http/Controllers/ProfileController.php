8790614fa1928d7d7dcf700637e041729caf995c
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-16 14:15:46 +0800

    committed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+// use App\Models\User;
+
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     //
     public function show($id){
-        $record = User::findOrFail($id);
+        $record = Student::findOrFail($id);
 
         return view('profile.show', compact('record'));
     }
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,8 @@
 <x-filament::page>
     <div style="display: flex; align-items: center; padding: 20px; border: 1px solid #e0e0e0; border-radius: 15px; background: linear-gradient(135deg, #fdfdfd, #f3f3f3); box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);">
         <div style="flex: 1; text-align: center;">
+
+            {{-- {{dd($record)}} --}}
             <img src="{{ $record->profile_image ? url($record->profile_image) : url('default_images/me.jpg') }}" alt="Profile Image" style="border-radius: 50%; width: 150px; height: 150px; object-fit: cover; border: 3px solid #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s ease-in-out;">
         </div>
         <div style="flex: 2; padding-left: 20px; display: flex; flex-direction: column; justify-content: center;">
