4ed611264cfb6694c5193abe043768e0c5cd9923
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-16 14:15:53 +0800

    committed

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,7 +5,7 @@
             {{-- {{dd($record)}} --}}
             <img src="{{ $record->profile_image ? url($record->profile_image) : url('default_images/me.jpg') }}" alt="Profile Image" style="border-radius: 50%; width: 150px; height: 150px; object-fit: cover; border: 3px solid #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s ease-in-out;">
         </div>
-        <div style="flex: 2; padding-left: 20px; display: flex; flex-direction: column; justify-content: center;">
+        <div x      xstyle="flex: 2; padding-left: 20px; display: flex; flex-direction: column; justify-content: center;">
             <h2 style="margin: 0; font-size: 26px; color: #333; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">{{ $record->first_name }} {{ $record->middle_name }}</h2>
             <p style="margin: 10px 0 5px 0; font-size: 18px; color: #777; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Age: {{ $record->age }}</p>
             <p style="margin: 5px 0; font-size: 18px; color: #777; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Gender: {{ $record->gender }}</p>
