df0968ca8df271e6461daaf533a2cbc54b6ae517
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-17 18:42:44 +0800

    commited

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             //     'primary' => Color::Amber,
             // ])
             ->brandLogo(asset('/default_images/MLG_Logo.png'))
-            ->brandLogoHeight('4rem')
+            ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
