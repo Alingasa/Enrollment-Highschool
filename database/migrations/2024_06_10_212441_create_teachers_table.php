de497d93eaa7686773a4a80109629b0624837b39
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-18 04:44:06 +0800

    awh

--- database/migrations/2024_06_10_212441_create_teachers_table.php
+++ database/migrations/2024_06_10_212441_create_teachers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teacher', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->integer('status')->default(1);
             // $table->foreignId('subject_id')->constrained()->cascadeOnDelete();
