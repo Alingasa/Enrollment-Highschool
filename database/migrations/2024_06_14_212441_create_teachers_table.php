d4f2f56ca1acd4cd5b1f93a7c8e04503a14a8e27
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-18 04:48:48 +0800

    awh

--- database/migrations/2024_06_14_212441_create_teachers_table.php
+++ database/migrations/2024_06_14_212441_create_teachers_table.php
@@ -11,43 +11,43 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teachers', function (Blueprint $table) {
-            $table->id();
-            $table->integer('status')->default(1);
-            // $table->foreignId('subject_id')->constrained()->cascadeOnDelete();
-            // $table->foreignId('strand_id')->nullable()->constrained()->cascadeOnUpdate();
-            // $table->string('grade_level')->nullable();
-            $table->string('school_id')->unique()->nullable();
-            $table->string('first_name');
-            $table->string('middle_name')->nullable();
-            $table->string('last_name');
-            $table->string('email')->unique()->nullable();
-            $table->date('birthdate');
-            $table->string('gender');                               // select from datalist
-            $table->integer('civil_status')->nullable();                        // enum
-            $table->string('contact_number')->nullable();
-            $table->string('religion')->nullable();                 // might select from datalist (not strict)
-            $table->string('facebook_url')->nullable();
+        // Schema::create('teachers', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->integer('status')->default(1);
+        //     // $table->foreignId('subject_id')->constrained()->cascadeOnDelete();
+        //     // $table->foreignId('strand_id')->nullable()->constrained()->cascadeOnUpdate();
+        //     // $table->string('grade_level')->nullable();
+        //     $table->string('school_id')->unique()->nullable();
+        //     $table->string('first_name');
+        //     $table->string('middle_name')->nullable();
+        //     $table->string('last_name');
+        //     $table->string('email')->unique()->nullable();
+        //     $table->date('birthdate');
+        //     $table->string('gender');                               // select from datalist
+        //     $table->integer('civil_status')->nullable();                        // enum
+        //     $table->string('contact_number')->nullable();
+        //     $table->string('religion')->nullable();                 // might select from datalist (not strict)
+        //     $table->string('facebook_url')->nullable();
 
-            /**
-             * Address
-             */
-            $table->string('purok')->nullable();
-            $table->string('sitio_street')->nullable();
-            $table->string('barangay')->nullable();
-            $table->string('municipality')->nullable();
-            $table->string('province')->nullable();
-            $table->integer('zip_code')->nullable();
+        //     /**
+        //      * Address
+        //      */
+        //     $table->string('purok')->nullable();
+        //     $table->string('sitio_street')->nullable();
+        //     $table->string('barangay')->nullable();
+        //     $table->string('municipality')->nullable();
+        //     $table->string('province')->nullable();
+        //     $table->integer('zip_code')->nullable();
 
-            // $table->string('guardian_name')->nullable();        //parent or guardian name;
-            // $table->string('LRN')->nullable();
+        //     // $table->string('guardian_name')->nullable();        //parent or guardian name;
+        //     // $table->string('LRN')->nullable();
 
-            $table->string('profile_image')->nullable();
+        //     $table->string('profile_image')->nullable();
 
 
-            $table->softDeletes();
-            $table->timestamps();
-        });
+        //     $table->softDeletes();
+        //     $table->timestamps();
+        // });
 
 
     }
